e5e793922090c9c18b4dff58695fcbd1c9d7126f
yashwanth <yashwanthbolishetty18@gmail.com>
2026-07-14 16:55:02 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
